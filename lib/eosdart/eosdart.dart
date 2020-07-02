bd078ae040868b1017307a65dc8fd7ac0767a1e6
paco <picc2711@usherbrooke.ca>
2020-07-02 11:33:48 -0400

    Add eosDart lib

---
+++ lib/eosdart/eosdart.dart
@@ -0,0 +1,17 @@
+/// Support for EOS API lib in Dart
+///
+/// More dartdocs go here.
+library eosdart;
+
+export 'src/models/account.dart';
+export 'src/models/node_info.dart';
+export 'src/models/block.dart';
+export 'src/models/action.dart';
+export 'src/models/action_block.dart';
+export 'src/models/transaction.dart';
+export 'src/models/primary_wrapper.dart';
+export 'src/models/block_header_state.dart';
+export 'src/models/abi.dart';
+
+// for dart-esr
+export 'src/serialize.dart';
