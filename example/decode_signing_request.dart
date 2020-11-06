27efd5b7de3fa744aea9d9540770234acf63a384
nik <nheger@gmail.com>
2020-11-06 18:26:02 +0800

    elaborated on example

--- example/decode_signing_request.dart
+++ example/decode_signing_request.dart
@@ -2,12 +2,40 @@ import 'package:dart_esr/dart_esr.dart';
 
 main(List<String> args) => decodeExample();
 
-void decodeExample() {
+void decodeExample() async {
+
+  var esr =
+      'esr://gmN0S9_Eeqy57zv_9xn9eU3hL_bxCbUs-jptJqsXY3-JtawgA0NBEFfzSoWzDAwM4bo2Z88yMjJAABOUVoQJGKw8nWa1MrnkhZQgmM8S7OrqEgxkABUAAA';
+
+  var request = SigningRequestManager.from(esr,
+      options: defaultSigningRequestEncodingOptions(
+          nodeUrl: 'https://api.eos.miami'));
+
+  var abis = await request.fetchAbis();
+
+  var auth = Authorization();
+  auth.actor = 'illumination';
+  auth.permission = 'active';
+
+  var actions = request.resolveActions(abis, auth);
+  print(" actions: " + actions?.toString());
+}
+
+void decodeExample2() async {
   var esr =
       'esr://gmNcs7jsE9uOP6rL3rrcvpMWUmN27LCdleD836_eTzFz-vCSjQEMXhmEFohe6ry3yuguIyNEiIEJSgvCBA58nnUl1dgwlAEoAAA';
-  var request = SigningRequestManager.from(esr,
-      options:
-          defaultSigningRequestEncodingOptions(nodeUrl: 'https://eos.eosn.io'));
 
-  print('decode\n' + request?.data?.toString());
+  var request = SigningRequestManager.from(esr,
+      options: defaultSigningRequestEncodingOptions(
+          nodeUrl: 'https://api.eos.miami'));
+
+  var abis = await request.fetchAbis();
+
+  var auth = Authorization();
+  auth.actor = 'illumination';
+  auth.permission = 'active';
+
+  var actions = request.resolveActions(abis, auth);
+  print(" actions: " + actions?.toString());
 }
+
